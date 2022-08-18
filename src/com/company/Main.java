b12761af595936b95e1c10844c4f3105910abe0b
Hooda Armaan <hooda.armaan@syk.fi>
2022-08-18 20:10:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int A = 12;
+        int B = 12;
+        if (A == B)
+        {
+            System.out.println((A + B) * (A + B));
+        }
+        else
+        {
+            System.out.println(A + B);
+        }
+    }
+}
